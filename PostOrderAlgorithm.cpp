c8ef4df062efb689fd76d30de753de21d9e55b9c
manpreetSingh1308 <92472735+manpreetSingh1308@users.noreply.github.com>
2021-10-30 17:13:52 +0530

    Newer variables
    It gives better understanding of codes to person one who wants to understand it.

--- PostOrderAlgorithm.cpp
+++ PostOrderAlgorithm.cpp
@@ -5,72 +5,72 @@ using namespace std;
 /* A binary tree node has data, pointer to left child
 and a pointer to right child */
 struct Node {
-	int data;
-	struct Node *left, *right;
-	Node(int data)
+	int datax;
+	struct Node *lefter, *righter;
+	Node(int datax)
 	{
-		this->data = data;
-		left = right = NULL;
+		this->datax = datax;
+		lefter = righter = NULL;
 	}
 };
 
 /* Given a binary tree, print its nodes according to the
 "bottom-up" postorder traversal. */
-void printPostorder(struct Node* node)
+void printPostorder(struct Node* node1)
 {
-	if (node == NULL)
+	if (node1 == NULL)
 		return;
 
 	// first recur on left subtree
-	printPostorder(node->left);
+	printPostorder(node1->lefter);
 
 	// then recur on right subtree
-	printPostorder(node->right);
+	printPostorder(node1->righter);
 
 	// now deal with the node
-	cout << node->data << " ";
+	cout << node1->datax << " ";
 }
 
 /* Given a binary tree, print its nodes in inorder*/
-void printInorder(struct Node* node)
+void printInorder(struct Node* node1)
 {
-	if (node == NULL)
+	if (node1 == NULL)
 		return;
 
 	/* first recur on left child */
-	printInorder(node->left);
+	printInorder(node1->lefter);
 
 	/* then print the data of node */
-	cout << node->data << " ";
+	cout << node1->datax << " ";
 
 	/* now recur on right child */
-	printInorder(node->right);
+	printInorder(node1->righter);
 }
 
 /* Given a binary tree, print its nodes in preorder*/
-void printPreorder(struct Node* node)
+void printPreorder(struct Node* node1)
 {
-	if (node == NULL)
+	if (node1 == NULL)
 		return;
 
 	/* first print data of node */
-	cout << node->data << " ";
+	cout << node1->datax << " ";
 
 	/* then recur on left subtree */
-	printPreorder(node->left);
+	printPreorder(node1->lefter);
 
 	/* now recur on right subtree */
-	printPreorder(node->right);
+	printPreorder(node1->righter);
 }
 
 /* Driver program to test above functions*/
 int main()
 {
 	struct Node* root = new Node(1);
-	root->left = new Node(2);
-	root->right = new Node(3);
-	root->left->left = new Node(4);
-	root->left->right = new Node(5);
+	root->lefter = new Node(2);
+	root->righter = new Node(3);
+	root->lefter->lefter = new Node(4);
+	root->lefter->righter = new Node(5);
 
 	cout << "\nPreorder traversal of binary tree is \n";
 	printPreorder(root);

e039ac0f35fa9d8f4a5f0c0d50f2089016bef0c4
Rachana <71558587+rachana1009@users.noreply.github.com>
2022-10-02 02:13:02 -0700

    Update fibonacciseries.cpp

--- fibonacciseries.cpp
+++ fibonacciseries.cpp
@@ -4,39 +4,29 @@
 #include <iostream>
 using namespace std;
 
-int checking(int num)
-{
-    if (num <= 1)
-        return 0;
-    for (int i = 2; i < num; i++)
-        if (num % i == 0)
-            return 1;
-    return 0;
-}
-//the main calling function...
-int main()
-{
-    int i, number;
-    int first1 = 0, second = 1;
-    int newterm = first1 + second;
-    cin >> number;
-    cout << second << " ";
-    for (i = 3; i <= number + 1; ++i)
-    {
-        if (newterm % 5 == 0)
-        {
-            cout << 0 << " ";
+int main() {
+    int n, t1 = 0, t2 = 1, nextTerm = 0;
+
+    cout << "Enter the number of terms: ";
+    cin >> n;
+
+    cout << "Fibonacci Series: ";
+
+    for (int i = 1; i <= n; ++i) {
+        // Prints the first two terms.
+        if(i == 1) {
+            cout << t1 << ", ";
+            continue;
         }
-        else if (checking(newterm) == 0)
-        {
-            cout << 0 << " ";
+        if(i == 2) {
+            cout << t2 << ", ";
+            continue;
         }
-        else
-        {
-            cout << newterm << " ";
-        }
-        first1 = second;
-        second = newterm;
-        newterm = second + first1;
+        nextTerm = t1 + t2;
+        t1 = t2;
+        t2 = nextTerm;
+        
+        cout << nextTerm << ", ";
     }
+    return 0;
 }
